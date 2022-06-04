0f4c12dc29d1b82adb924ad0dee3ab3b83017e5a
Mauricio Gonzatto <mauriciogonzatto@hotmail.com>
2022-06-04 17:29:53 -0300

    DataContext Dependancy Injection

--- API/Controllers/ValuesController.cs
+++ API/Controllers/ValuesController.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Domain;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
 
 namespace API.Controllers
 {
@@ -10,17 +13,27 @@ namespace API.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+
+        private readonly DataContext _context;
+
+        public ValuesController(DataContext context)
         {
-            return new string[] {"value1", "value2"};
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Value>>> Get()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
         }
 
         // GET api/value/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public async Task<ActionResult<Value>> Get(int id)
         {
-            return "value";
+            var value = await _context.Values.FindAsync(id);
+            return Ok(value);
         }
 
         // POST
