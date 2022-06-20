15ad8687ba19fb268add8718ed44a1648d306ca6
Mauricio Gonzatto <mauriciogonzatto@hotmail.com>
2022-06-19 22:20:13 -0300

    Delete Handler

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,20 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Application.Activities;
 using MediatR;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Persistence;
 
 namespace API
@@ -42,6 +33,7 @@ namespace API
             services.AddMediatR(typeof(Details.Handler).Assembly);
             services.AddMediatR(typeof(Create.Handler).Assembly);
             services.AddMediatR(typeof(Edit.Handler).Assembly);
+            services.AddMediatR(typeof(Delete.Handler).Assembly);
             
             services.AddControllers();                        
         }
