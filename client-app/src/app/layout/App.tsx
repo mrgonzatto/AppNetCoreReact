b0b407ccffd35e2b280ce675af5a15623bf965b4
mrgonzatto <mauriciogonzatto@hotmail.com>
2022-11-05 13:50:48 -0300

    Initialising the form with data

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -5,14 +5,19 @@ import { IActivity } from './models/activity';
 import NavBar from '../../features/nav/NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 
-//function App() {
 const App = () => {
 
   const [activities, setActivities] = useState<IActivity[]>([])
   const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(null);
+  const [editMode, setEditMode] = useState(false);
 
   const handleSelectActivity = (id: string ) => {
     setSelectedActivity(activities.filter(a => a.id === id)[0])
+  }
+
+  const handleOpenCreateForm = () => {
+    setSelectedActivity(null);
+    setEditMode(true);
   }
 
   useEffect(() => {    
@@ -22,19 +27,21 @@ const App = () => {
         setActivities(response.data)
       });
   }, []);
-
  
-    return (
-      <Fragment>
-        <NavBar />
-        <Container style={{marginTop:'7em'}}>
-          <ActivityDashboard 
-            activities={activities} 
-            selectActivity={handleSelectActivity} 
-            selectedActivity={selectedActivity}/>
-        </Container>
-      </Fragment>
-    );
+  return (
+    <Fragment>
+      <NavBar openCreateForm={handleOpenCreateForm} />
+      <Container style={{marginTop:'7em'}}>
+        <ActivityDashboard 
+          activities={activities} 
+          selectActivity={handleSelectActivity} 
+          selectedActivity={selectedActivity} 
+          editMode={editMode}
+          setEditMode={setEditMode} 
+          setSelectedActivity={setSelectedActivity}/>
+      </Container>
+    </Fragment>
+  );
 }
 
 export default App;
